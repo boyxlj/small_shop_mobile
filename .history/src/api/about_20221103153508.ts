04e7217cdc81ce22d98bbdd6117549a4bfffe57e
Miraitowa <x709500@163.com>
2022-11-03 15:55:26 +0800

    修改reademe文件

--- .history/src/api/about_20221103153508.ts
+++ .history/src/api/about_20221103153508.ts
@@ -1,3 +1,3 @@
-const BASEURL = "http://localhost:3303/api"
+const BASEURL = "https://api.helloxlj.top/api"
 import Axios from "./index"
 export const  getAbout = ()=>Axios.get(`${BASEURL}/select/article/small/shop?artId=small-shop-about`)
